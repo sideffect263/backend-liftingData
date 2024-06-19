3bdd086aae372b288123b2e0a90930d5e20ab9a1
ariel biton <arielbiton03@gmail.com>
2024-06-19 14:59:22 +0300

    server_on

--- server.js
+++ server.js
@@ -40,6 +40,8 @@ app.get('/data/:year', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log(`Server running on port`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
